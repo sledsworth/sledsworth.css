5c91494d32773e3df0a6657bc8dee43e40e8b2c4
Steve Ledsworth <sledsworth@gmail.com>
2020-06-16 20:35:00 -0400

    created announcement banner for pages other than home

--- .eleventy.js
+++ .eleventy.js
@@ -1,101 +1,101 @@
-const { DateTime } = require("luxon");
-const fs = require("fs");
-const pluginRss = require("@11ty/eleventy-plugin-rss");
-const pluginSyntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
-const pluginNavigation = require("@11ty/eleventy-navigation");
-const markdownIt = require("markdown-it");
-const markdownItAnchor = require("markdown-it-anchor");
+const { DateTime } = require('luxon');
+const fs = require('fs');
+const pluginRss = require('@11ty/eleventy-plugin-rss');
+const pluginSyntaxHighlight = require('@11ty/eleventy-plugin-syntaxhighlight');
+const pluginNavigation = require('@11ty/eleventy-navigation');
+const markdownIt = require('markdown-it');
+const markdownItAnchor = require('markdown-it-anchor');
 
 module.exports = function(eleventyConfig) {
-  eleventyConfig.addPlugin(pluginRss);
-  eleventyConfig.addPlugin(pluginSyntaxHighlight);
-  eleventyConfig.addPlugin(pluginNavigation);
+	eleventyConfig.addPlugin(pluginRss);
+	eleventyConfig.addPlugin(pluginSyntaxHighlight);
+	eleventyConfig.addPlugin(pluginNavigation);
 
-  eleventyConfig.setDataDeepMerge(true);
+	eleventyConfig.setDataDeepMerge(true);
 
-  eleventyConfig.addLayoutAlias("post", "layouts/post.njk");
+	eleventyConfig.addLayoutAlias('post', 'layouts/post.njk');
 
-  eleventyConfig.addFilter("readableDate", dateObj => {
-    return DateTime.fromJSDate(dateObj, {zone: 'utc'}).toFormat("LLL dd, yyyy");
-  });
+	eleventyConfig.addFilter('noWhiteSpace', string => {
+		console.log(string);
+		return string.replace(/\s/g, '');
+	});
 
-  // https://html.spec.whatwg.org/multipage/common-microsyntaxes.html#valid-date-string
-  eleventyConfig.addFilter('htmlDateString', (dateObj) => {
-    return DateTime.fromJSDate(dateObj, {zone: 'utc'}).toFormat('yyyy-LL-dd');
-  });
+	eleventyConfig.addFilter('readableDate', dateObj => {
+		return DateTime.fromJSDate(dateObj, { zone: 'utc' }).toFormat('LLL dd, yyyy');
+	});
 
-  // Get the first `n` elements of a collection.
-  eleventyConfig.addFilter("head", (array, n) => {
-    if( n < 0 ) {
-      return array.slice(n);
-    }
+	// https://html.spec.whatwg.org/multipage/common-microsyntaxes.html#valid-date-string
+	eleventyConfig.addFilter('htmlDateString', dateObj => {
+		return DateTime.fromJSDate(dateObj, { zone: 'utc' }).toFormat('yyyy-LL-dd');
+	});
 
-    return array.slice(0, n);
-  });
+	// Get the first `n` elements of a collection.
+	eleventyConfig.addFilter('head', (array, n) => {
+		if (n < 0) {
+			return array.slice(n);
+		}
 
-  eleventyConfig.addCollection("tagList", require("./_11ty/getTagList"));
-  eleventyConfig.addCollection("archiveByMonth", require("./_11ty/getArchiveByMonth"));
+		return array.slice(0, n);
+	});
 
-  eleventyConfig.addPassthroughCopy("img");
-  eleventyConfig.addPassthroughCopy("css");
+	eleventyConfig.addCollection('tagList', require('./_11ty/getTagList'));
+	eleventyConfig.addCollection('archiveByMonth', require('./_11ty/getArchiveByMonth'));
 
-  /* Markdown Overrides */
-  let markdownLibrary = markdownIt({
-    html: true,
-    breaks: true,
-    linkify: true
-  }).use(markdownItAnchor, {
-    permalink: true,
-    permalinkClass: "direct-link",
-    permalinkSymbol: "#"
-  });
-  eleventyConfig.setLibrary("md", markdownLibrary);
+	eleventyConfig.addPassthroughCopy('img');
+	eleventyConfig.addPassthroughCopy('css');
 
-  // Browsersync Overrides
-  eleventyConfig.setBrowserSyncConfig({
-    callbacks: {
-      ready: function(err, browserSync) {
-        const content_404 = fs.readFileSync('_site/404.html');
+	/* Markdown Overrides */
+	let markdownLibrary = markdownIt({
+		html: true,
+		breaks: true,
+		linkify: true,
+	}).use(markdownItAnchor, {
+		permalink: true,
+		permalinkClass: 'direct-link',
+		permalinkSymbol: '#',
+	});
+	eleventyConfig.setLibrary('md', markdownLibrary);
 
-        browserSync.addMiddleware("*", (req, res) => {
-          // Provides the 404 content without redirect.
-          res.write(content_404);
-          res.end();
-        });
-      },
-    },
-    ui: false,
-    ghostMode: false
-  });
+	// Browsersync Overrides
+	eleventyConfig.setBrowserSyncConfig({
+		callbacks: {
+			ready: function(err, browserSync) {
+				const content_404 = fs.readFileSync('_site/404.html');
 
-  return {
-    templateFormats: [
-      "md",
-      "njk",
-      "html",
-      "liquid"
-    ],
+				browserSync.addMiddleware('*', (req, res) => {
+					// Provides the 404 content without redirect.
+					res.write(content_404);
+					res.end();
+				});
+			},
+		},
+		ui: false,
+		ghostMode: false,
+	});
 
-    // If your site lives in a different subdirectory, change this.
-    // Leading or trailing slashes are all normalized away, so don’t worry about those.
+	return {
+		templateFormats: ['md', 'njk', 'html', 'liquid'],
 
-    // If you don’t have a subdirectory, use "" or "/" (they do the same thing)
-    // This is only used for link URLs (it does not affect your file structure)
-    // Best paired with the `url` filter: https://www.11ty.io/docs/filters/url/
+		// If your site lives in a different subdirectory, change this.
+		// Leading or trailing slashes are all normalized away, so don’t worry about those.
 
-    // You can also pass this in on the command line using `--pathprefix`
-    // pathPrefix: "/",
+		// If you don’t have a subdirectory, use "" or "/" (they do the same thing)
+		// This is only used for link URLs (it does not affect your file structure)
+		// Best paired with the `url` filter: https://www.11ty.io/docs/filters/url/
 
-    markdownTemplateEngine: "liquid",
-    htmlTemplateEngine: "njk",
-    dataTemplateEngine: "njk",
+		// You can also pass this in on the command line using `--pathprefix`
+		// pathPrefix: "/",
 
-    // These are all optional, defaults are shown:
-    dir: {
-      input: ".",
-      includes: "_includes",
-      data: "_data",
-      output: "_site"
-    }
-  };
+		markdownTemplateEngine: 'liquid',
+		htmlTemplateEngine: 'njk',
+		dataTemplateEngine: 'njk',
+
+		// These are all optional, defaults are shown:
+		dir: {
+			input: '.',
+			includes: '_includes',
+			data: '_data',
+			output: '_site',
+		},
+	};
 };
