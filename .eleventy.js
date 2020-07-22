8d09ebbdde55c17d669b406c6b60bbd7a4f12821
Steve Ledsworth <sledsworth@gmail.com>
2020-07-22 08:21:47 -0400

    added footnotes, started font sizing changes

--- .eleventy.js
+++ .eleventy.js
@@ -5,6 +5,7 @@ const pluginSyntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
 const pluginNavigation = require("@11ty/eleventy-navigation");
 const markdownIt = require("markdown-it");
 const markdownItAnchor = require("markdown-it-anchor");
+const markdownFootnotes = require("markdown-it-footnote");
 const CleanCSS = require("clean-css");
 
 module.exports = function (eleventyConfig) {
@@ -65,7 +66,7 @@ module.exports = function (eleventyConfig) {
     breaks: true,
     linkify: true,
   })
-    .use(require("markdown-it-footnote"))
+    .use(markdownFootnotes)
     .use(markdownItAnchor, {
       permalink: true,
       permalinkClass: "post-header-link",
